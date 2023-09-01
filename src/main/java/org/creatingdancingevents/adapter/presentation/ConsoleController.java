6e010c4d5f614a4b71e16226bd590fdf9cab271d
Oliver Zihler <ozihler@gmail.com>
2023-09-01 17:07:05 +0200

    refactor

--- src/main/java/org/creatingdancingevents/adapter/presentation/ConsoleController.java
+++ src/main/java/org/creatingdancingevents/adapter/presentation/ConsoleController.java
@@ -10,46 +10,53 @@ import java.util.Scanner;
 
 
 public class ConsoleController {
-    private final CreateDancingEvent useCase;
+    private final CreateDancingEvent createDancingEvent;
     Scanner scanner;
     PrintStream out;
 
     public ConsoleController(Scanner scanner, PrintStream out, CreateDancingEvent createDancingEvent) {
         this.scanner = scanner;
         this.out = out;
-        useCase = createDancingEvent;
+        this.createDancingEvent = createDancingEvent;
     }
 
     public void execute() {
-        boolean continueOrFinish;
         do {
-            out.println("Please enter the details for your event:");
-            out.println("Event name:");
-            String title = scanner.nextLine();
-
-            out.println("Event description:");
-            String description = scanner.nextLine();
-
-            out.println("Event organizer id:");
-            String eventOrganizer = scanner.nextLine();
-
-            DancingEventRecord input = new DancingEventRecord(null, title, description, eventOrganizer);
-
-            PresentCreatedDancingEvent presentSuccess = dancingEvent -> {
-                out.println("Your event has been created with id: " + dancingEvent.id());
-                out.println("Here are the details:");
-                out.println("Title: " + dancingEvent.title());
-                out.println("Description: " + dancingEvent.description());
-                out.println("Event organizer: " + dancingEvent.eventOrganizerId());
-            };
-            PresentDancingEventCreationFailure presentFailure = e -> out.println("There was an error creating your event: " + e.getMessage());
-
-            useCase.execute(input, presentSuccess, presentFailure);
-
-            out.println("Would you like to create another event? (true/false)");
-            continueOrFinish = scanner.nextLine().equals("true");
-        } while (continueOrFinish);
+            DancingEventRecord input = collectInputFromCommandLine();
+            createDancingEvent.execute(input, successPresenter(), errorPresenter());
+        } while (askUserToContinueOrFinish());
     }
 
+    private DancingEventRecord collectInputFromCommandLine() {
+        out.println("Please enter the details for your event:");
+        out.println("Event name:");
+        String title = scanner.nextLine();
 
+        out.println("Event description:");
+        String description = scanner.nextLine();
+
+        out.println("Event organizer id:");
+        String eventOrganizer = scanner.nextLine();
+
+        return new DancingEventRecord(null, title, description, eventOrganizer);
+    }
+
+    private PresentCreatedDancingEvent successPresenter() {
+        return dancingEvent -> {
+            out.println("Your event has been created with id: " + dancingEvent.id());
+            out.println("Here are the details:");
+            out.println("Title: " + dancingEvent.title());
+            out.println("Description: " + dancingEvent.description());
+            out.println("Event organizer: " + dancingEvent.eventOrganizerId());
+        };
+    }
+
+    private PresentDancingEventCreationFailure errorPresenter() {
+        return e -> out.println("There was an error creating your event: " + e.getMessage());
+    }
+
+    private boolean askUserToContinueOrFinish() {
+        out.println("Would you like to create another event? (true/false)");
+        return scanner.nextLine().equals("true");
+    }
 }
