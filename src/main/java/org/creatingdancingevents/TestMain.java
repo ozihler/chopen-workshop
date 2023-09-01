fecdd88922d7d6000079adcc10ae1288734a4e51
Oliver Zihler <ozihler@gmail.com>
2023-09-01 17:00:47 +0200

    inline

--- src/main/java/org/creatingdancingevents/TestMain.java
+++ src/main/java/org/creatingdancingevents/TestMain.java
@@ -9,15 +9,11 @@ import java.util.Scanner;
 public class TestMain {
 
     public static void main(String[] args) {
-        executeCreateDancingEventUseCase();
-
-    }
-
-    private static void executeCreateDancingEventUseCase() {
         var repo = new InMemoryDancingEventRepository();
         var useCase = new CreateDancingEventUseCase(repo, repo);
         var console = new ConsoleController(new Scanner(System.in), System.out, useCase);
 
         console.execute();
     }
+
 }
