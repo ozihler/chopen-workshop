d7904b9e06a8350d126d5e9348664beda8ebf4a4
Oliver Zihler <ozihler@gmail.com>
2023-09-03 18:14:09 +0200

    add first test.

--- src/main/java/org/test_create_dancing_events/domain/UnpublishedDancingEvents.java
+++ src/main/java/org/test_create_dancing_events/domain/UnpublishedDancingEvents.java
@@ -3,6 +3,7 @@ package org.test_create_dancing_events.domain;
 import org.test_create_dancing_events.domain.exception.NumberOfUnpublishedDancingEventsReachedMaxNumberException;
 
 import java.util.List;
+import java.util.Objects;
 
 public class UnpublishedDancingEvents {
     private static final int MAX_NUMBER_OF_UNPUBLISHED_DANCING_EVENTS = 5;
@@ -29,5 +30,18 @@ public class UnpublishedDancingEvents {
         return "UnpublishedDancingEvents{" +
                 "unpublishedDancingEvents=" + unpublishedDancingEvents +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UnpublishedDancingEvents that = (UnpublishedDancingEvents) o;
+        return Objects.equals(unpublishedDancingEvents, that.unpublishedDancingEvents);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(unpublishedDancingEvents);
     }
 }
